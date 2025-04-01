86eb75ce46131760d7736f3f797f8fd8d65dafab
Pankaj <pankaj.php@jeenasikho.co.in>
2025-04-01 10:50:32 +0530

    add pdf

---
+++ aps.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" /> -->
+    <link href="https://fonts.cdnfonts.com/css/calibri-light" rel="stylesheet">
+    <title>Document</title>
+
+    <style>
+        body {
+          font-size: 13px;
+          font-family: 'Calibri Light', sans-serif;
+        }
+        h1 {
+            font-size: 24px;
+            font-family: 'Calibri Light', sans-serif;
+        }
+        p {
+            font-size: 11px;
+            font-family: 'Calibri Light', sans-serif;
+        }
+
+        table tr td{
+            line-height: 0;
+            font-family: 'Calibri Light', sans-serif;
+            font-size: 11px;
+        }
+
+        table thead th{
+            line-height: 0;
+            font-family: 'Calibri Light', sans-serif;
+            font-size: 11px;
+        }
+        .description{
+            margin: 0px 50px;
+            padding: 3px;
+        }
+
+        .aurtho{
+            padding-left: 20px;
+
+        }
+
+    </style>
+</head>
+<body>
+    <div style='border: 1px solid #222; padding: 0px;'>
+        <table style="width: 100%;">
+            <tr>
+                <td style="text-align: left;">GSTIN : ABC4545GTFG66</td>
+                <td style="text-align: left;">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;GST INVOICE</td>
+                <td style="text-align: right;">
+                    Mob:- 9874563210
+                    <br />
+                    9874563210
+                </td>
+            </tr>
+        </table>
+        <h1 style="text-align: center;">XYZ AHTEGPRVIED</h1>
+        <p class="description" style="text-align: center; background: #222; color: #fff; font-size: 15px; text-transform: uppercase; font-size: 11px;"><b>There are many variations of passages of Lorem Ipsum available</b></p>
+        <p style='text-align: center; padding: 0 20px 0 20px;'>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical.</p>
+        <table style="width: 100%; border-top: 1px solid #222;">
+            <tr>
+                <td style="text-align: left;">Invoice No.: <b style='font-size: 20px;'>2</b></td>
+                <td style="text-align: right;">Date : 20-03-2025</td>
+            </tr>
+        </table>
+        <table style="width: 100%; border-top: 1px solid #222;">
+            <tr>
+                <td style="text-align: left;">State 04</td>
+                <td style="text-align: right;">Reverse Charge : Yes/No</td>
+            </tr>
+        </table>
+        <table style="width: 100%; border-top: 1px solid #222; border-collapse: collapse;">
+            <tr>
+                <td style="border-right: 1px solid #222; text-align: left; padding: 5px; margin: 0;"><b>M/s</b> : Lorem Ipsum Lorem Ipsum</td>
+                <td style="text-align: right; padding: 5px;"><b>Transport Mode</b> : N/A</td>
+            </tr>
+            <tr>
+                <td style="border-right: 1px solid #222; text-align: left; padding: 5px; margin: 0;"><b>GST</b> : AGTFGTAFA64488484hh</td>
+                <td style="text-align: right; padding: 5px;"><b>Vichal No</b> : N/A</td>
+            </tr>
+            <tr>
+                <td style="border-right: 1px solid #222; text-align: left; padding: 5px; margin: 0;"><b>State Code</b> : 04</td>
+                <td style="text-align: right; padding: 5px;"><b>Place of Supply</b> : Lorem Ipsum Lorem Ipsum </td>
+            </tr>
+        </table>
+        <table style="width: 100%; border-collapse: collapse;">
+            <thead>
+                <tr>
+                    <th style="text-align: left; border-top: 1px solid #222; border-right: 1px solid #222; padding: 10px;">
+                        Sr.<br />No.
+                    </th>
+                    <th style="text-align: left; border-top: 1px solid #222; border-right: 1px solid #222; padding: 10px;">Description Of Goods</th>
+                    <th style="text-align: left; border-top: 1px solid #222; border-right: 1px solid #222; padding: 10px;">HSN Code</th>
+                    <th style="text-align: left; border-top: 1px solid #222; border-right: 1px solid #222; padding: 10px;">Qty</th>
+                    <th style="text-align: left; border-top: 1px solid #222; border-right: 1px solid #222; padding: 10px;">Rate</th>
+                    <th style="text-align: left; border-top: 1px solid #222; padding: 10px;">Amount</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td style="text-align: left; border-top: 1px solid #222; border-right: 1px solid #222; padding: 10px;">1</td>
+                    <td style="text-align: left; border-top: 1px solid #222; border-right: 1px solid #222; padding: 10px;">There are many variations</td>
+                    <td style="text-align: left; border-top: 1px solid #222; border-right: 1px solid #222; padding: 10px;">4587458</td>
+                    <td style="text-align: left; border-top: 1px solid #222; border-right: 1px solid #222; padding: 10px;">1</td>
+                    <td style="text-align: left; border-top: 1px solid #222; border-right: 1px solid #222; padding: 10px;">5054</td>
+                    <td style="text-align: left; border-top: 1px solid #222; padding: 10px;">5054</td>
+                </tr>
+                <tr>
+                    <td style="text-align: left; border-top: 1px solid #222; border-right: 1px solid #222; padding: 10px;">2</td>
+                    <td style="text-align: left; border-top: 1px solid #222; border-right: 1px solid #222; padding: 10px;">There are many variations</td>
+                    <td style="text-align: left; border-top: 1px solid #222; border-right: 1px solid #222; padding: 10px;">4587458</td>
+                    <td style="text-align: left; border-top: 1px solid #222; border-right: 1px solid #222; padding: 10px;">1</td>
+                    <td style="text-align: left; border-top: 1px solid #222; border-right: 1px solid #222; padding: 10px;">5054</td>
+                    <td style="text-align: left; border-top: 1px solid #222; padding: 10px;">5054</td>
+                </tr>
+                <tr>
+                    <td style="text-align: left; border-top: 1px solid #222; border-right: 1px solid #222; padding: 10px;">3</td>
+                    <td style="text-align: left; border-top: 1px solid #222; border-right: 1px solid #222; padding: 10px;">There are many variations</td>
+                    <td style="text-align: left; border-top: 1px solid #222; border-right: 1px solid #222; padding: 10px;">4587458</td>
+                    <td style="text-align: left; border-top: 1px solid #222; border-right: 1px solid #222; padding: 10px;">1</td>
+                    <td style="text-align: left; border-top: 1px solid #222; border-right: 1px solid #222; padding: 10px;">5054</td>
+                    <td style="text-align: left; border-top: 1px solid #222; padding: 10px;">5054</td>
+                </tr>
+                <tr>
+                    <td style="text-align: left; border-top: 1px solid #222; border-right: 1px solid #222; padding: 10px;"></td>
+                    <td colspan="3" style="text-align: center; border-top: 1px solid #222; border-right: 1px solid #222; padding: 10px;">Total</td>
+                    <td colspan="2" style="text-align: center; border-top: 1px solid #222; padding: 10px;">10542</td>
+                </tr>
+                <tr>
+                    <td style="text-align: left; border-top: 1px solid #222; border-right: 1px solid #222; padding: 10px;"></td>
+                    <td colspan="1" style="text-align: right; border-top: 1px solid #222; border-right: 1px solid #222; padding: 10px;">
+                        <b>CGST@ 9%</b>
+                        <br />
+                        <b>SGST@ 9%</b>
+                    </td>
+                    <td style="text-align: center; border-top: 1px solid #222; border-right: 1px solid #222; padding: 10px;">
+                        255.66
+                        <br/>
+                        255.66
+                    </td>
+                    <td style="text-align: center; border-top: 1px solid #222; border-right: 1px solid #222; padding: 10px;" ></td>
+                    <td colspan="2" style="text-align: center; border-top: 1px solid #222; padding: 10px;">548.32</td>
+                </tr>
+                <tr>
+                    <td style="text-align: left; border-top: 1px solid #222; border-right: 1px solid #222; padding: 10px;"></td>
+                    <td colspan="1" style="text-align: right; border-top: 1px solid #222; border-right: 1px solid #222; padding: 10px;">
+                        <b>Grand Total</b>
+                    </td>
+                    <td style="text-align: center; border-top: 1px solid #222; border-right: 1px solid #222; padding: 10px;" ></td>
+                    <td style="text-align: left; border-top: 1px solid #222; border-right: 1px solid #222; padding: 10px;">
+                        <b>3</b>
+                    </td>
+                    <td colspan="2" style="text-align: center; border-top: 1px solid #222; padding: 10px;"><b>11542</b></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <div>
+        <div style="border-top: 1px solid #222;">
+            <p>Total Amount in Words : Five Thousand Five Hundred Twenty Two Only :-</p>
+        </div>
+        <div style="text-align: left;" class="bankdetails">
+            <p>
+                <b>Bank Name</b>&nbsp;&nbsp;&nbsp;: &nbsp;&nbsp;&nbsp;Canara Bank
+                <br/>
+                <b>Account No.</b>&nbsp;&nbsp;: &nbsp;&nbsp;&nbsp;152548584585
+                <br/>
+                <b>IFSC Code</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: &nbsp;&nbsp;&nbsp;CANA18767
+            </p>
+        </div>
+        <table style="width: 100%; border-collapse: collapse;">
+            <tr>
+                <td>
+                    <h5>Term & Condition</h5>
+                    <ol>
+                        <li>Subject to Chandigarh Jurisdiction.</li>
+                        <li>Good one solid  will not be taken back.</li>
+                        <li>Our responsibility ceases once when the goods leave our premises.</li>
+                        <li>Interest @ 24% will be changed if payment not made within 7 days.</li>
+                    </ol>
+                </td>
+                <td style="text-align: end;" class="aurtho">
+                    <table style='width: 100%; border-collapse: collapse;'>
+                        <tbody>
+                            <tr>
+                                <td style="text-align: center;" class="aurthoDesc"> 
+                                    <p>Certified that the particuler given above are true and correct</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="text-align: center;">
+                                    For XYZ HYTRFVTYVDE
+                                </td>
+                            </tr>
+                            <tr>
+                                <td  style="padding-top: 80px;" >Customer Sign</td>
+                                <td  style="padding-top: 80px;" >Auth. Signatory</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </td>
+            </tr>
+        </table>
+        <p><b>This is a Computer Generated Invoice.</b></p>
+    </div>
+
+
+</body>
+</html>
